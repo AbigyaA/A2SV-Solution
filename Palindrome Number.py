67865541d701f3df66a5ad669ab41437a6631162
Abigya Ayele <81214866+AbigyaA@users.noreply.github.com>
2024-04-04 21:31:41 +0300

    add solution for Palindrome Number

---
+++ Palindrome Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        arr=[y for y in str(x)]
+        j=0
+        k=-1
+        n=len(arr)//2
+        for i in range(n):
+            if arr[j] != arr[k]:
+                return False
+            j+=1
+            k-=1
+        return True
